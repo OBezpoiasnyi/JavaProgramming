6fa6f6eb8ee9cac883aaea113218c7d7de12d97d
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-13 10:48:58 -0600

    day38_ParkingLot

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,15 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "Gray", 123456);
+
+        Tesla tesla = new Tesla("Model S", 2021, 46000, "White", 11111);
+
+        BMW bmw = new BMW("X5", 2020, 67350, "black", 12000);
+
+
+    }
+}
