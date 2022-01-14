2dd0bb6a5ea1f76ed1305b7fe174a708ceb7ba08
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-14 10:06:18 -0600

    day39_Recap

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -34,4 +34,13 @@ public class Shape {
         return 0;
     }
 
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
