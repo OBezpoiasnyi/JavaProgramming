37ff35950a13577e1bfb0b6b6df68a0221a35954
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-14 10:13:36 -0600

    day39_Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -3,7 +3,23 @@ package day39_Recap.shapeTask;
 public class Square extends Shape{
 
 
-    public Square(String name) {
+    private double side;
+
+
+    public double getSide() {
+        return side;
+    }
+    public void setSide(double side) {
+        if(side<=0 ){
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+
+    public Square(String name, double side) {
         super(name);
+        setSide(side);
     }
 }
