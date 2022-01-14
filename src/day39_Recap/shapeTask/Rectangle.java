5031becee9ff4b4825a08ff0096fd9f4ebf4e1a8
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-14 10:47:59 -0600

    day39_Recap

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -33,4 +33,25 @@ public class Rectangle extends Shape{
         setLength(length);
         setWidth(width);
     }
+
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * ( length + width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
