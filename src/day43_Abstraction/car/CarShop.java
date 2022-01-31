fde793a2175e443bb1511ae7bc810b0fa0a4309c
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-31 14:01:32 -0600

    day42_Abstraction_Intro

--- src/day43_Abstraction/car/CarShop.java
+++ src/day43_Abstraction/car/CarShop.java
@@ -5,6 +5,32 @@ public class CarShop {
     public static void main(String[] args) {
 
 
+       // Car car1 = new Car("Cydeo", "25", "Blue", 2022, 100000);
+            // we can not create object from the abstract class!!!
 
+        Honda honda = new Honda("Accord", "Black", 2019, 30000);
+
+        Audi audi = new Audi("Q7", "White", 2020, 45000);
+
+        Tesla tesla = new Tesla("Model 3", "Blue", 2021, 60000);
+
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
+
+
+        honda.setColor("Red");
+        audi.setColor("Pink");
+        tesla.setColor("Green");
+
+
+        honda.setPrice(33333);
+        audi.setPrice(44444);
+        tesla.setPrice(55555);
+
+
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
     }
 }
