bba6efef85b5c73a4cd07562687dd60f842934aa
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-31 14:49:05 -0600

    day42_Abstraction_Intro

---
+++ src/day43_Abstraction/employee/Employee.java
@@ -0,0 +1,17 @@
+package day43_Abstraction.employee;
+
+public abstract class Employee {
+
+
+    public String jobTitle;
+    public int id;
+    public double salary;
+
+
+
+
+
+    public abstract void work();
+
+
+}
