b8c6f8aa989c1bf62b9cc633b5822d7a9e642010
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-01-31 15:35:47 -0600

    day42_Abstraction_Intro

--- src/day43_Abstraction/employee/Employee.java
+++ src/day43_Abstraction/employee/Employee.java
@@ -1,17 +1,59 @@
 package day43_Abstraction.employee;
 
-public abstract class Employee {
+public abstract class Employee extends Person{
 
 
-    public String jobTitle;
-    public int id;
-    public double salary;
+    private String jobTitle;
+    private final int id;
+    private double salary;
 
 
+    public Employee(String name, int age, char gender, String jobTitle, int id, double salary) {
+        super(name, age, gender);
+        setJobTitle(jobTitle);
+        if(id<=0){
+            throw new RuntimeException("Invalid ID: " + id);
+        }
+        this.id = id;
+        setSalary(salary);
+    }
 
 
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public void setJobTitle(String jobTitle) {
+        if(jobTitle.isEmpty()||jobTitle.isBlank()){
+            throw new RuntimeException("Invalid Job Title: " + jobTitle);
+        }
+        this.jobTitle = jobTitle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        if (salary<=0){
+            throw new RuntimeException("Invalid salary: " + salary);
+        }
+        this.salary = salary;
+    }
 
     public abstract void work();
 
 
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\''+
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/day43_Abstraction/employee/Tester.java
+++ src/day43_Abstraction/employee/Tester.java
@@ -1,4 +1,23 @@
 package day43_Abstraction.employee;
 
 public class Tester extends Employee{
+
+
+    public Tester(String name, int age, char gender, String jobTitle, int id, double salary) {
+        super(name, age, gender, jobTitle, id, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName() + " is working in company as a " + getJobTitle());
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " sleeps 8 hours");
+    }
+
+    public void bugReport(){
+        System.out.println(getName() + " is creating bug reports");
+    }
 }
